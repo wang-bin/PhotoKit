2da5e158867cc5b4d0d21c441126b1ac98d5f335
wang-bin <wbsecg1@gmail.com>
2012-07-09 02:13:52 +0800

    image info dialog

---
+++ src/tools/ImageInfoDialog.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+	ImageInfoDialog.h: description
+	Copyright (C) 2012 Wang Bin <wbsecg1@gmail.com>
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+******************************************************************************/
+
+
+#ifndef IMAGEINFODIALOG_H
+#define IMAGEINFODIALOG_H
+
+#include "Dialog.h"
+
+namespace PhotoKit {
+class ImageInfoDialogPrivate;
+class ImageInfoDialog : public Dialog
+{
+	Q_OBJECT
+	Q_DECLARE_PRIVATE(ImageInfoDialog)
+public:
+	explicit ImageInfoDialog(QGraphicsScene *scene, QGraphicsItem *parent = 0);
+	~ImageInfoDialog();
+
+	void setBaseImageInfo(const QString& text);
+	void setExifInfo(const QString& text);
+	void setGPSInfo(const QString& text);
+
+	void showBaseInfo();
+protected:
+	//ImageInfoDialog(ImageInfoDialogPrivate& d, QGraphicsScene *scene, QGraphicsItem *parent = 0);
+	
+private slots:
+	void showInfo();
+private:
+	//ImageInfoDialogPrivate* d_ptr;
+};
+
+} //namespace PhotoKit
+
+#endif // IMAGEINFODIALOG_H
